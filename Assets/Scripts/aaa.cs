3158e50853c70ce778945ada3eff24b4b3722f69
hiroki-kitahara <kitahara@1923.co.jp>
2016-12-06 11:29:27 +0900

    計算系の関数を追加

--- Assets/Scripts/aaa.cs
+++ Assets/Scripts/aaa.cs
@@ -5,5 +5,21 @@ namespace GitTest
 {
     public sealed class aaa : MonoBehaviour
     {
+        int Add(int a, int b)
+        {
+            return a + b;
+        }
+        int Division(int a, int b)
+        {
+            return a / b;
+        }
+        int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        int Multi(int a, int b)
+        {
+            return a * b;
+        }
     }
 }
